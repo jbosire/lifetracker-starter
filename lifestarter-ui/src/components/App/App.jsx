9850c37a953ed8b75d439c443a7e37111aaed148
Joram Bosire <jbosire@jbosire-ltm73m6.internal.salesforce.com>
2022-07-06 16:24:44 -0700

    Create file structure

---
+++ lifestarter-ui/src/components/App/App.jsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
+
+
+export default function App() {
+  return (
+    <div className="app">joram</div>
+  )
+}
+ 

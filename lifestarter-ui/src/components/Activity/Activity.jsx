6f5b8a1df9ebe12a8bae858877db0dc7d3c2f8f6
Joram Bosire <jbosire@jbosire-ltm73m6.internal.salesforce.com>
2022-07-07 14:01:21 -0700

    Added general pages layouts

--- lifestarter-ui/src/components/Activity/Activity.jsx
+++ lifestarter-ui/src/components/Activity/Activity.jsx
@@ -1,13 +1,30 @@
 import * as React from "react";
-import Navbar from "../Navbar/Navbar";
 import "./Activity.css";
 import { useState, useEffect } from "react";
 
 export default function Activity() {
-   
   return (
     <div className="Activity">
-     
+      <div className="Banner">
+        <h1>Activity</h1>
+      </div>
+      <div className="content">
+        <div className="ActivityOverview">
+          <div className="header">
+            <h3>Overview</h3>
+            <button className="Button outline small outline aqua">
+              {" "}
+              Add Activity
+            </button>
+          </div>
+          <div className="feed">
+            
+            <div className="empty">
+              <h2>Nothing here yet.</h2>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- lifestarter-ui/src/components/Sleep/Sleep.jsx
+++ lifestarter-ui/src/components/Sleep/Sleep.jsx
@@ -1,13 +1,30 @@
 import * as React from "react";
-import Navbar from "../Navbar/Navbar";
 import "./Sleep.css";
 import { useState, useEffect } from "react";
 
 export default function Sleep() {
-   
   return (
     <div className="Sleep">
-   
+      <div className="Banner">
+        <h1>Sleep</h1>
+      </div>
+      <div className="content">
+        <div className="SleepOverview">
+          <div className="header">
+            <h3>Overview</h3>
+            <button className="Button outline small outline aqua">
+              {" "}
+              Add sleep
+            </button>
+          </div>
+          <div className="feed">
+            
+            <div className="empty">
+              <h2>Nothing here yet.</h2>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

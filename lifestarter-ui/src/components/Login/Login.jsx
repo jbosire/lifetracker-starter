c12acd019077fd719bfe3164031e6ecc6bac1d25
Joram Bosire <jbosire@jbosire-ltm73m6.internal.salesforce.com>
2022-07-07 00:26:05 -0700

    created register page

--- lifestarter-ui/src/components/Login/Login.jsx
+++ lifestarter-ui/src/components/Login/Login.jsx
@@ -7,6 +7,25 @@ export default function Login() {
    
   return (
     <div className="Login">
+      <div className="card">
+        <h2>Login</h2>
+        <br/>
+        <div className="form">
+          <div className="input-field">
+            <label for="email">Email</label>
+            <input type="email" name="email" placeholder="doe@joram.com"/>
+          </div>
+          <div className="input-field">
+            <label for="password">Password</label>
+            <input type="password" name="password" placeholder="password"/>
+          </div>
+
+          <button className="btn">Login</button>
+        </div>
+        <div className="footer">
+          <p>Not registered yet? Sign up {<a href="/register"> here.</a>}</p>
+        </div>
+      </div>
      
     </div>
   );
--- lifestarter-ui/src/components/Register/Register.jsx
+++ lifestarter-ui/src/components/Register/Register.jsx
@@ -4,10 +4,71 @@ import "./Register.css";
 import { useState, useEffect } from "react";
 
 export default function Register() {
-   
   return (
     <div className="Register">
-    
+      <div className="card">
+        <h2>Sign Up</h2>
+        <br />
+        <div className="form">
+          <div className="input-field">
+            <label for="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              placeholder="Enter valid email eg. doe@joram.com"
+            />
+          </div>
+          <div className="split-input-field">
+            <div className="input-field">
+              <label for="firstName">First Name</label>
+              <input
+                type="text"
+                name="firstName"
+                placeholder="Enter your first name"
+              />
+            </div>
+
+            <div className="input-field">
+              <label for="lastName"> Last Name </label>
+              <input
+                type="text"
+                name="lastName"
+                placeholder="Enter your last name"
+              />
+            </div>
+          </div>
+          <div className="input-field">
+            <label for="username">Username</label>
+            <input type="text" name="username" placeholder="your username" />
+          </div>
+
+          <div className="input-field">
+            <label for="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Enter password"
+            />
+          </div>
+
+          <div className="input-field">
+            <label for="passwordConfirm">Confirm Password</label>
+            <input
+              type="password"
+              name="passwordConfirm"
+              placeholder="Confirm your password"
+            />
+          </div>
+          <button className="btn"> Create Account</button>
+        </div>
+
+        <div className="footer">
+          <p>
+            Have an account? Login
+            <a href="/login"> here.</a>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
